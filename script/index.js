ad2864565016493c7f9e854f86bfed2806494ea9
a-h-taju <ahosanhabibtaju789@gmail.com>
2025-10-23 20:34:07 +0600

    Fourth commit

--- script/index.js
+++ script/index.js
@@ -16,13 +16,25 @@ const loadWordLevel = (id) => {
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById('wordLevel')
     wordContainer.innerHTML = ""
+
+    if(words.length == 0){
+        wordContainer.innerHTML = `
+                <div class="text-center col-span-full py-10 space-y-6 font-bangla">
+            <img src="./assets/alert-error.png" alt="" class="mx-auto">
+            <p class="font-medium text-xl text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+            </p>
+            <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান।</h2>
+        </div>   
+        `
+    }
+
     words.forEach((word) => {
         const card = document.createElement("div")
         card.innerHTML = `
                 <div class="bg-white rounded-xl text-center py-10 px-5 space-y-4 shadow-sm">
-                    <h1 class="text-xl font-bold">${word.word}</h1>
+                    <h1 class="text-xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h1>
                     <p class="font-semibold text-sm">Pronounciation <i class="fa-regular fa-circle-right"></i> Meaning</p>
-                    <div class="font-bangla text-2xl font-medium">${word.pronunciation} <i class="fa-regular fa-circle-right"></i> ${word.meaning}</div>
+                    <div class="font-bangla text-2xl font-medium">${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি"} <i class="fa-regular fa-circle-right"></i> ${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"}</div>
                     <div class="flex justify-between">
                         <button class="bg-[#1A91FF10] hover:bg-[#1A91FF80]  px-5 py-3 rounded-xl"><i class="fa-solid fa-circle-info"></i></button>
                         <button class="bg-[#1A91FF10] hover:bg-[#1A91FF80] px-5 py-3 rounded-xl"><i class="fa-solid fa-volume-high"></i></button>
